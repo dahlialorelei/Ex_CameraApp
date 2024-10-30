fa52195f8f4128203b336d85f1035e1a94e53c7e
dahlialorelei <113202608+dahlialorelei@users.noreply.github.com>
2024-10-30 19:18:51 -0400

    Initial Commit

---
+++ TestCameraApp/TestCameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCameraAppApp.swift
+//  TestCameraApp
+//
+//  Created by Dahlia Taylor on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct TestCameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestCameraAppTests/TestCameraAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestCameraAppTests.swift
+//  TestCameraAppTests
+//
+//  Created by Dahlia Taylor on 2024-10-30.
+//
+
+import Testing
+@testable import TestCameraApp
+
+struct TestCameraAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
